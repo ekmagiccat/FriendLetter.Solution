c2e52312976d8f5265ad69b6a487ee2ef4422540
Eva Kemp <eva.j.kemp@gmail.com>
2023-07-17 15:33:57 -0700

    first commit

---
+++ FriendLetter/Models/LetterVariable.cs
@@ -0,0 +1,13 @@
+namespace FriendLetter.Models
+{
+    public class LetterVariable
+    {
+        public string Recipient { get; set; }
+        public string Sender { get; set; }
+        public string Spot { get; set; }
+
+        public string DiffSpot { get; set; }
+        public string Souvenir { get; set; }
+
+    }
+}
